0f766abada2fbcd33554007058999c3161484a41
YapZanan <naufalsetiawan64@gmail.com>
2025-01-11 11:01:16 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
+
 REPO_DIR="/home/bersama/Naufal/placeholder-api"
 
+# Docker container name
 CONTAINER_NAME="placeholder-server"
+
 
 cd $REPO_DIR || exit 1
 
+
+git stash --include-untracked
+
+
 git fetch origin
+
 
 LOCAL_COMMIT=$(git rev-parse @)
 REMOTE_COMMIT=$(git rev-parse @{u})
@@ -14,7 +22,9 @@ if [ "$LOCAL_COMMIT" != "$REMOTE_COMMIT" ]; then
 
     git pull origin main
 
+
     docker-compose build placeholder-server
+
 
     docker-compose up -d placeholder-server
 
@@ -22,3 +32,5 @@ if [ "$LOCAL_COMMIT" != "$REMOTE_COMMIT" ]; then
 else
     echo "No changes detected. No action taken."
 fi
+
+git stash pop
